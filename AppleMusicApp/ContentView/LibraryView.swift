6957ed0e687aa0c6f774749541fcd7b3c012d23b
Garitskiy Victor <dinomc4711@gmail.com>
2023-04-19 23:31:08 +0600

    add mainScreen

---
+++ AppleMusicApp/ContentView/LibraryView.swift
@@ -0,0 +1,52 @@
+//
+//  LibraryView.swift
+//  AppleMusicApp
+//
+//  Created by Victor Garitskyu on 19.04.2023.
+//
+
+import SwiftUI
+
+struct LibraryView: View {
+    var body: some View {
+        TabView {
+                    NavigationView {
+                        VStack {
+                            Spacer()
+                            Spacer()
+                            PlayerView()
+                                .overlay(Divider(), alignment: .bottom)
+                        }
+                        .navigationTitle("Медиатека")
+                        .toolbar {
+                            NavigationLink(
+                                destination: PlayerView(),
+                                label: {
+                                    Text("Править")
+                                        .foregroundColor(.red)
+                                }
+                            )
+                        }
+                    }.tabItem {
+                        Image(systemName: "play.square.stack")
+                        Text("Музыка")
+                    }
+                    Text("Радио")
+                        .tabItem {
+                            Image(systemName: "dot.radiowaves.left.and.right")
+                            Text("Радио")
+                        }
+                    Text("Поиск")
+                        .tabItem {
+                            Image(systemName: "magnifyingglass.circle.fill")
+                            Text("Поиск")
+                        }
+                }.accentColor(.red)
+            }
+        }
+
+struct LibraryView_Previews: PreviewProvider {
+    static var previews: some View {
+        LibraryView()
+    }
+}
