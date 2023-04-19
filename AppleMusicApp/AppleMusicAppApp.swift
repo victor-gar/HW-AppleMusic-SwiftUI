f1df4e05420f0ac0f012fef516ab8e23c9df13e6
Garitskiy Victor <dinomc4711@gmail.com>
2023-04-19 22:54:56 +0600

    Initial Commit

---
+++ AppleMusicApp/AppleMusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicAppApp.swift
+//  AppleMusicApp
+//
+//  Created by Victor Garitskyu on 19.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
